97ca1d9b6f06a142a7e4605974c00ed5bf9dd73e
Victor Suzdalev <victor.suzdalev@gmail.com>
2015-05-29 02:03:31 +0300

    Invisible textareas

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 var Card = React.createClass({
     render() {
         return (
-            <div className='card'>{this.props.model.label}</div>
+            <div className='card'>
+                <label for="area">{this.props.model.label}</label>
+                <textarea id="area" value={this.props.model.value}></textarea>
+            </div>
         )
     }
 })
