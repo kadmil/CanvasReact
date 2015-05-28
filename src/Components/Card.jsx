ba68da9e00598d738483ed16af3e8e14fd87d03e
Victor Suzdalev <victor.suzdalev@gmail.com>
2015-05-29 02:25:29 +0300

    React htmlFor for labellign

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,8 +4,8 @@ var Card = React.createClass({
     render() {
         return (
             <div className='card'>
-                <label for="area">{this.props.model.label}</label>
-                <textarea id="area" value={this.props.model.value}></textarea>
+                <label htmlFor='area'>{this.props.model.label}</label>
+                <textarea id='area' value={this.props.model.value}></textarea>
             </div>
         )
     }
